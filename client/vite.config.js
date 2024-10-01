a46cd4ba1824d6dd383ed2cba2f91a3d1789eefc
india-99s <india99swebsite@gmail.com>
2024-10-01 10:08:14 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:8000",
+        target: "http://api.india99s.com",
         secure: false,
       }
     }
