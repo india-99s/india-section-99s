51277dca093ea8405aa7ca9a989baf2f68536ede
india-99s <india99swebsite@gmail.com>
2024-10-04 11:23:02 +0530

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
   }, [location.search]);
 
   return (
-    <div className='min-h-screen flex flex-col md:flex-row pt-[18%]'>
+    <div className='min-h-screen flex flex-col md:flex-row pt-[28%] sm:pt-[10%] '>
       {tab === 'member' && <DashMember />}
       {tab === 'gallery' && <DashGallery />}
       {tab === 'event' && <DashEvent />}
